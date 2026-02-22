a391c4e23094d392d295f7d46336c3edcfa5e4c6
maheshwar-besra <besra.m9@gmail.com>
2026-02-22 22:17:04 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "e4a-class-v20260222-2"; // Change v1 to v2 when you update the site
+const CACHE_NAME = "e4a-class-v20260222-3"; // Change v1 to v2 when you update the site
 
 // Files to cache immediately
 const ASSETS = [
